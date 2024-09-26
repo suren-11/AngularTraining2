e5658c8fb033fe8a98dabff6d66f2d5afaa9e8f7
suren-11 <presanth11@gmail.com>
2024-09-26 22:35:35 +0530

    generated app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InBaseComponent } from './in-base/in-base.component';
 import { ExBaseComponent } from './ex-base/ex-base.component';
@@ -14,7 +12,6 @@ import { ExBaseComponent } from './ex-base/ex-base.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [
     provideClientHydration()
