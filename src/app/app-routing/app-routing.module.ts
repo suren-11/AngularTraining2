5fcab33357993098884c44147e2fe874129f7030
suren-11 <presanth11@gmail.com>
2024-09-27 09:05:27 +0530

    add the routing parts to the routing module

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,10 +4,11 @@ import { InBaseComponent } from '../in-base/in-base.component';
 import { ExBaseComponent } from '../ex-base/ex-base.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
+import { RegistrationModule } from '../registration/registration.module';
 
 const routes: Routes = [
   { path: 'login', component: ExBaseComponent, children: [{ path: '', component: LoginComponent }] },
-  { path: 'reg', component: InBaseComponent },
+  { path: 'reg', component: InBaseComponent, loadChildren: ()=> import('../registration/registration.module').then(m=> m.RegistrationModule) },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AddNewStudentComponent } from '../add-new-student/add-new-student.component';
+import { ShowAllStudentsComponent } from '../show-all-students/show-all-students.component';
 
-
+const routes: Routes = [
+  {path:'add-new-student', component:AddNewStudentComponent},
+  {path:'show-all-students', component:ShowAllStudentsComponent},
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports:[RouterModule]
 })
 export class RegistrationModule { }
